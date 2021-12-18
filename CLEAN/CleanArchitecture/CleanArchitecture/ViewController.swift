5e199153b7294e3432ba2aa8434da451ec24ae3d
Betzer09 <betzer.austin09@gmail.com>
2021-12-18 10:41:24 -0700

    Added .gitignore

---
+++ CLEAN/CleanArchitecture/CleanArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanArchitecture
+//
+//  Created by Austin Betzer on 12/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
